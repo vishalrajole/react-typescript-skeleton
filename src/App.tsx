386a9bb5022dba490bbb5c7e45898c984ad19878
vishalrajole <vishalrajole1991@gmail.com>
2022-07-22 07:44:19 +0200

    added eslint for ts and tsx files only

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,44 @@
-export const App = () => <h1>Hello React typescript! </h1>;
+import { useEffect, useState } from 'react';
+import { IntlProvider } from 'react-intl';
+import Dummy from './components/Dummy';
+
+const LOCALE = 'fr';
+const DEFAULT_LOCALE = 'en';
+
+export async function getMessages(locale = 'default') {
+  try {
+    const messages = await import(`./i18n/${locale}.json`);
+    return messages.default;
+  } catch (error) {
+    throw new Error(`Application couldn't find ${locale}.json`);
+  }
+}
+
+export const App = () => {
+  const [messages, setMessages] = useState<any>(); // for now any
+
+  useEffect(() => {
+    const getMessages = async (locale = 'default') => {
+      try {
+        const messages = await import(`./i18n/${locale}.json`);
+        setMessages(messages.default);
+      } catch (error) {
+        throw new Error(`Application couldn't find ${locale}.json`);
+      }
+    };
+    getMessages(LOCALE);
+  }, []);
+
+  return (
+    <>
+      <IntlProvider
+        messages={messages}
+        locale={LOCALE}
+        defaultLocale={DEFAULT_LOCALE}
+      >
+        <h1>Hello React typescript! </h1>
+        <Dummy />
+      </IntlProvider>
+    </>
+  );
+};
